0994b53720a016e99fc781176ef8655fa3008a05
Dávilos Tavares da Costa <davilos@apsinformatica.com.br>
2025-01-09 16:36:05 -0300

    feat: adiciona componentes e layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <>
-      <ThemeProvider theme={defaultTheme}>
-        <Button variant="primary" />
-        <Button variant="secondary" />
-        <Button variant="success" />
-        <Button variant="danger" />
-        <Button />
+      <BrowserRouter>
+        <ThemeProvider theme={defaultTheme}>
+          <Router />
 
-        <GlobalStyle />
-      </ThemeProvider>
+          <GlobalStyle />
+        </ThemeProvider>
+      </BrowserRouter>
     </>
   )
 }
